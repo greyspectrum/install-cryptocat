45dcf351e1e5c44f5d602481e693a8745df3564d
greyspectrum <greyspectrum@users.noreply.github.com>
2016-10-18 15:48:51 -0400

    Added fingerprint checking functionality.

--- install-cryptocat.sh
+++ install-cryptocat.sh
@@ -12,10 +12,25 @@
 cd
 
 # Fetch Cryptocat signing keys
+echo "==Fetching Cryptocat signing keys..."
 
 gpg --recv-keys --keyserver keys.gnupg.net 0xab266cb76091b1f8
 
-# Install Cryptocat
+# Verifying Cryptocat signing key fingerprint
+echo "==Verifying Cryptocat signing key fingerprint..."
+
+echo -e 'pub   3072R/6091B1F8 2016-08-09\n      Key fingerprint = FA21 CD53 6312 FADF 9B5D  D804 AB26 6CB7 6091 B1F8\nuid                  Nadim Kobeissi <nadim@nadim.computer>\nsub   3072R/A4D437B0 2016-08-09' > fingerprint
+
+if gpg --fingerprint 0xab266cb76091b1f8 | diff -q fingerprint
+    echo "==The Cryptocat signing key provided by the keyserver, 0x6091B1F8, has the expected fingerprint. Key Fingerprint: FA21 CD53 6312 FADF 9B5D  D804 AB26 6CB7 6091 B1F8"
+else
+    echo "==ERROR: THE KEY PROVIDED BY THE KEYSERVER HAS RETURNED AN UNEXPECTED FINGERPRINT. ABORTING..."
+    rm fingerprint
+    exit 1
+fi
+
+# Verify Cryptocat
+echo "==Verifying download..."
 
 mkdir .cryptocat
 
@@ -26,9 +41,9 @@ curl -O https://download.crypto.cat/client/Cryptocat-linux-x64.zip
 unzip Cryptocat-linux-x64.zip
 
 if gpg --verify Cryptocat.zip.asc Cryptocat.zip; then
-    echo "This download has been verified. GPG signature check successful."
+    echo "==This download has been verified. GPG signature check successful."
 else
-    echo "ERROR: BAD GPG SIGNATURE. ABORTING..."
+    echo "==ERROR: BAD GPG SIGNATURE. ABORTING..."
     cd
     rm -rf .cryptocat
     exit 1
@@ -41,8 +56,11 @@ unzip Cryptocat.zip
 rm Cryptocat-linux-x64.zip Cryptocat.zip.asc Cryptocat.zip
 
 # Create startup script
+echo "==Creating startup script... To launch Cryptocat, you can use the graphical user interface or just type cryptocat in the terminal. Please wait..."
 
 cd
+
+rm fingerprint
 
 if test -e ~/bin; then
     echo -e '#!/usr/bin/env bash \n\n# Start up Cryptocat\n\ncd\n\ncd .cryptocat/Cryptocat-linux-x64\n\n./Cryptocat' > ~/bin/cryptocat
