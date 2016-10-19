a39fd79cf3ca8fff2b8113206ca9c00bdaff0b76
greyspectrum <greyspectrum@users.noreply.github.com>
2016-10-19 00:11:35 -0400

    edited user facing info

--- install-cryptocat.sh
+++ install-cryptocat.sh
@@ -12,25 +12,25 @@
 cd
 
 # Fetch Cryptocat signing keys
-echo "==Fetching Cryptocat signing keys..."
+echo -e '\n==Fetching Cryptocat signing keys...'
 
 gpg --recv-keys --keyserver keys.gnupg.net 0xab266cb76091b1f8
 
 # Verifying Cryptocat signing key fingerprint
-echo "==Verifying Cryptocat signing key fingerprint..."
+echo -e '\n==Verifying Cryptocat signing key fingerprint...'
 
 echo -e 'pub   3072R/6091B1F8 2016-08-09\n      Key fingerprint = FA21 CD53 6312 FADF 9B5D  D804 AB26 6CB7 6091 B1F8\nuid                  Nadim Kobeissi <nadim@nadim.computer>\nsub   3072R/A4D437B0 2016-08-09\n' > fingerprint
 
 if gpg --fingerprint 0xab266cb76091b1f8 | diff -q fingerprint -; then
-    echo -e '==Verified. The Cryptocat signing key provided by the keyserver, 0x6091B1F8, has the expected fingerprint.\nKey Fingerprint: FA21 CD53 6312 FADF 9B5D  D804 AB26 6CB7 6091 B1F8'
+    echo -e '\n==Verified. The Cryptocat signing key provided by the keyserver, 0x6091B1F8, has the expected fingerprint.\nKey Fingerprint: FA21 CD53 6312 FADF 9B5D  D804 AB26 6CB7 6091 B1F8'
 else
-    echo "==ERROR: THE KEY PROVIDED BY THE KEYSERVER HAS RETURNED AN UNEXPECTED FINGERPRINT. ABORTING..."
+    echo -e '\n==ERROR: THE KEY PROVIDED BY THE KEYSERVER HAS RETURNED AN UNEXPECTED FINGERPRINT. ABORTING...'
     rm fingerprint
     exit 1
 fi
 
 # Download Cryptocat
-echo "==Download Cryptocat..."
+echo -e '\n==Downloading Cryptocat...'
 
 mkdir .cryptocat
 
@@ -41,12 +41,12 @@ curl -O https://download.crypto.cat/client/Cryptocat-linux-x64.zip
 unzip Cryptocat-linux-x64.zip
 
 # Verify Cryptocat
-echo "==Verifying Cryptocat download..."
+echo -e '\n==Verifying Cryptocat download...'
 
 if gpg --verify Cryptocat.zip.asc Cryptocat.zip; then
-    echo "==This download has been verified. GPG signature check successful."
+    echo -e '\n==This download has been verified. GPG signature check successful.'
 else
-    echo "==ERROR: BAD GPG SIGNATURE. ABORTING..."
+    echo -e '\n==ERROR: BAD GPG SIGNATURE. ABORTING...'
     cd
     rm -rf .cryptocat
     exit 1
@@ -59,7 +59,7 @@ unzip Cryptocat.zip
 rm Cryptocat-linux-x64.zip Cryptocat.zip.asc Cryptocat.zip
 
 # Create startup script
-echo "==Creating startup script... To launch Cryptocat, you can use the graphical user interface or just type cryptocat in the terminal. Please wait..."
+echo -e '\n==Creating startup script...\n==To launch Cryptocat, you can use the graphical user interface or just type cryptocat in the terminal.\n==Please wait...'
 
 cd
 
@@ -75,7 +75,7 @@ fi
 chmod +x ~/bin/cryptocat
 
 # Start Cryptocat
-echo "==Starting Cryptocat..."
+echo -e '\n==Starting Cryptocat...'
 
 cd .cryptocat/Cryptocat-linux-x64
 
